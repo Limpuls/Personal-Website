e3f1f093ad3454417ceca9eb09ce606353684499
Laimis <Limpuls17@gmail.com>
2017-12-04 02:23:57 +0100

    added projects

--- script.js
+++ script.js
@@ -1,10 +1,62 @@
 $(document).ready(function () {
-  $(".nav-link").click(function (e) {
-    e.preventDefault();
-    e.stopPropagation();
+  $("a").click(function (event) {
+    if ($(this).attr("href") != "https://github.com/Limpuls?tab=repositories") {
+        if ($(this).attr("href") != "https://limpuls.github.io/Javascript-Synth/") {
+      if ($(this).attr("href") != "resume.pdf") {
+
+    event.preventDefault();
+    event.stopPropagation();
+    console.log(this);
     var url = $(this).attr("href");
-    $("body").animate({
+    console.log(url);
+    console.log($("." + url.split("#")[1]).offset().top);
+    console.log($("." + url.split("#")[1]));
+    $("html, body").animate({
       scrollTop: $("." + url.split("#")[1]).offset().top
   }, 1000);
+}
+}
+};
+})
+
+var header = $(".home");
+var nav = $("nav");
+var cta = $(".cta");
+var h = $("h");
+var p = $("p");
+TweenLite.from(header, 1, {y: 1000});
+TweenLite.from(header, 1.5, {opacity: 100, delay: 0.9});
+TweenLite.from(nav, 2, {opacity: 0})
+TweenLite.from(cta, 1.3, {x: -400, delay: 0.80});
+TweenLite.from(h, 1.5, {x: -400});
+TweenLite.from(p, 1.7, {x: -400, delay: 0.1});
+
+var controller = new ScrollMagic.Controller();
+var ourScene = new ScrollMagic.Scene({
+  triggerElement: ".body_container",
+  duration: "60%"
+})
+  .setClassToggle(".body_container", "fade-in")
+  .addTo(controller);
+
+
+var pinScene = new ScrollMagic.Scene({
+  triggerElement: ".body_container",
+  triggerHook: 0,
+  duration: 120
+})
+  .setPin(".body_container", {})
+  .addTo(controller);
+
+
+/*
+$(".img_placeholder1").click(function() {
+  var p = document.createElement("p")
+  p.innerHTML = "zdareee";
+  $(".description").append(p);
+})
+*/
 });
-});
+//$('a[href$="ABC"]')...
+//console.log($(this).attr("href") == "resume.pdf")
+//console.log($('a[href$="resume.pdf"]').attr('href'))
